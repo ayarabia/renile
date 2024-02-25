59f4878e82c773b7ebfde9665b46454eefb558b9
ayarabia <ayarabia732@gmail.com>
2024-02-25 17:11:27 +0200

    add ponds

--- src/networking/baseInstance.js
+++ src/networking/baseInstance.js
@@ -3,6 +3,6 @@ export const instance = axios.create({
     baseURL: "http://159.65.161.165:80/",
     headers: {
       "Content-Type": "application/json",
-      Accept: "application/json",
+      
   },
 });
