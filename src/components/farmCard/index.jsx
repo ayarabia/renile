220aeaf678ada04a136d78c1cf85b37a162da975
ayarabia <ayarabia732@gmail.com>
2024-03-05 01:59:06 +0200

    edit feed rate

--- src/components/farmCard/index.jsx
+++ src/components/farmCard/index.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import location from '../../assets/location.svg'
 import farmImage from "../../assets/savannah.svg"
+import farm1 from "../../assets/farm1.jpeg"
+import farm2 from "../../assets/farm2.jpeg"
+import farm3 from "../../assets/farm3.jpeg"
 import { Link } from 'react-router-dom'
 
 function FarmCard(props) {   
@@ -9,10 +12,18 @@ function FarmCard(props) {
       sessionStorage.setItem('farmId', farmDetaile.id);
     }
     console.log(farmDetaile);
+    const addFarmImage=(id)=>{
+if(id==1){
+  return farm1
+}else if(id==2){
+return farm2
+}
+return farm3
+    }
   return (
 <Link  to='/dashboard'  state={{ name: farmDetaile.name ,id:farmDetaile.id}} onClick={storeFarmId}>
 <div className='bg-white rounded-[20px] shadow-3xl  '>
-       <img src={farmDetaile.logo !==null?farmDetaile.logo:farmImage} alt="farm image" className='block w-full  rounded-t-[20px]' />
+       <img src={addFarmImage(farmDetaile.id)} alt="farm image" className='block w-full  rounded-t-[20px]' />
        <div className='p-4 lg:h-[155px] h-auto'>
         <p className={`mb-2 text-base font-semibold ${farmDetaile.health?"text-primary":"text-[#F14F4F]"}`}> {farmDetaile.name}</p>
         <p className='flex items-start'>        <img src={location} alt="map" className='me-2 mt-[2px]' />
